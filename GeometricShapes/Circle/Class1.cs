d9bb51ad853bce74091378749eb0f85ac64fdd9f
MONICA PATRICIA GOMEZ ALVAREZ <monikgo154@gmail.com>
2026-08-31 20:28:23 -0500

    Geometric

---
+++ GeometricShapes/Circle/Class1.cs
@@ -0,0 +1,46 @@
+﻿
+namespace Circle
+{
+    public class Circle : GeometricFigure
+    {
+        //Fields
+
+        private double _r;
+
+        // Constructor
+        public Circle(string name, double r)
+        {
+            Name = name;
+            R = r;
+        }
+
+        public string Name { get; }
+
+        // Properties 
+
+        public double R
+        {
+            get => _r;
+            set => _r = ValidateR(value);
+        }
+
+        //Methods
+
+        public override double GetArea() => Math.PI * R * R;
+
+        public override double GetPerimeter() => 2 * Math.PI * R;
+
+        // Valides
+        private double ValidateR(double r)
+        {
+            if (r <= 0)
+            {
+                throw new ArgumentException("The radium of the circle is incorrect.");
+            }
+            return r;
+        }
+    }
+
+    public class GeometricFigure
+    {
+    }
---
+++ GeometricShapes/Kite/Rhombus.cs
@@ -0,0 +1,18 @@
+﻿namespace Kite
+{
+    public class Rhombus
+    {
+        private string name;
+        private double a;
+        private double d1;
+        private double d2;
+
+        public Rhombus(string name, double a, double d1, double d2)
+        {
+            this.name = name;
+            this.a = a;
+            this.d1 = d1;
+            this.d2 = d2;
+        }
+    }
+}
